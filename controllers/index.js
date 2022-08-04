c80140e06625df10c72e9167618e32540a20b5bf
vitor <vitorrcosta96@gmail.com>
2022-08-04 19:35:46 -0300

    [NEW] Connection with spotify API

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,42 @@
-exports.getIndex = (req, res, next) => {
-    res.render('index', { title: 'Express' });
+const axios = require('axios').default;
+const qs = require('querystring')
+
+var client_id = process.env.SPOTIFY_CLIENT_API_KEY;
+var client_secret = process.env.SPOTIFY_SECRET_API_KEY;
+
+exports.getIndex = async (req, res, next) => {
+  
+  const headers = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    auth: {
+      username: client_id,
+      password: client_secret,
+    },
+  };
+  const data = {
+    grant_type: 'client_credentials',
+  };
+
+  try {
+    const response = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      qs.stringify(data),
+      headers
+    );
+
+    const genresResponse = await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + response.data.access_token
+      }
+    })
+
+    res.send(genresResponse.data.genres)
+  } catch (error) {
+    console.log(error);
+    res.send(error)
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var indexController = require('../controllers/index.js');
 /* GET home page. */
-router.get('/', indexController.getIndex);
+router.get('/genres', indexController.getIndex);
 
 module.exports = router;
